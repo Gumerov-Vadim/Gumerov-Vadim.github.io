09c493b4e1235475d6ea501b21e94e5c6ab1c31e
Gumerov-Vadim <95773619+Gumerov-Vadim@users.noreply.github.com>
2024-01-30 23:25:02 +0500

    added data fetching and search

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import { useEffect,useState } from 'react';
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -20,8 +20,24 @@ const rows = [
 ];
 
 export default function App() {
+  const [table,setTable]=useState([])
+  const [input,setInput]=useState("")
+  useEffect(()=>{fetch("https://dummyjson.com/users").then((res)=>res.json()).then((res)=>{setTable(res)})},[])
+  useEffect(()=>{console.log(table)},[table])
+  function search(e){
+      const newValue = e.target.value;
+      setInput(newValue);
+      fetch(`https://dummyjson.com/users/search?q=${newValue}`)
+      .then(res => res.json())
+      .then(res=>setTable(res));
+  }
   return (
-    <TableContainer component={Paper}>
+    <>
+    <header>
+        <span>возьмите меня стажером :-) </span>
+        <input onChange={search} value={input}></input>
+    </header>
+    <TableContainer className='table' component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -33,22 +49,24 @@ export default function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+        {/* ФИО, возраст, пол, номер телефона и адрес (город и название улицы).  */}
+          {table?.users?.map((user) => (
             <TableRow
-              key={row.name}
+              key={user.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+              {user.firstName} {user.lastName} {user.maidenName}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{user.age}</TableCell>
+              <TableCell align="right">{user.gender}</TableCell>
+              <TableCell align="right">{user.phone}</TableCell>
+              <TableCell align="right">{user.address.city}, {user.address.address}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
+  </>
   );
 }
